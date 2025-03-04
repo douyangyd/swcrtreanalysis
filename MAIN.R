83c39ec2c451cb287f1eacf341381a9b4d0c04eb
douyangyd <dyou6561@gmail.com>
2025-03-04 18:40:36 -0500

    direct read files

--- MAIN.R
+++ MAIN.R
@@ -1,11 +1,12 @@
+
 # Set configuration
 cfg <- list(
   quiet = T,
   pkgs = c("dplyr", "msm", "lmerTest", "splines", "performance", "stringr",
-           "sandwich", "clubSandwich", "WoodburyMatrix", "steppedwedge"),
+           "sandwich", "clubSandwich", "WoodburyMatrix", "steppedwedge","htmltools","webshot","DT"),
   path = "G:/Shared drives/Stepped Wedge Data Files/Trials/",
   wd = "swcrtreanalysis",
-  ind = 23 # This corresponds to the datasets in dir(cfg$path)
+  ind = 25 # This corresponds to the datasets in dir(cfg$path)
   # ind = c(1:74)
 )
 
@@ -21,8 +22,8 @@ for (pkg in c(cfg$pkgs)) {
 }
 
 # Source necessary files
-setwd(cfg$wd)
-files <- c("Data_processing.R", "data_analysis.R", "helpers.R")
+setwd("G:/Shared drives/Stepped Wedge Data Files/Software/Data Analysis")
+files <- c("data_analysis.R", "helpers.R", "process results.R")
 for (file in files) { do.call("source", list(file)) }
 source("G:/Shared drives/Stepped Wedge Data Files/Software/Robust Variance/vcovCRglmerMod.R") # Temporary
 
@@ -32,15 +33,52 @@ for (i in cfg$ind) {
   data <- read.csv(paste0(
     cfg$path, dir(cfg$path)[i], "/Standardized data/trial_data.csv"
   ))
+  dataname <- print(dir(cfg$path)[i])
   head(data)
-  data <- process_data(data)
+  source("G:/Shared drives/Stepped Wedge Data Files/Software/Data Analysis/Data_processing.R") 
+  length(outcome_columns)
+  family
+  length(offset_columns)
+  design <- ifelse(length(ind_columns) == 0, "cs", "co")
+  offset <- ifelse(length(offset_columns) != 0, T, F)
+  drive_folder <- paste0(
+    cfg$path, dir(cfg$path)[i], "/Results"
+  )  # Windows
+  # drive_folder <- "~/Google Drive/Results/"  # Mac/Linux
   
-  test1 <- fit(
-    data = data[[1]],
-    family = "gaussian", 
-    design = "cs",
-    rve_type = "classic",
-    ss_correct = T
-  )
+  # Ensure the folder exists
+  if (!dir.exists(drive_folder)) dir.create(drive_folder, recursive = TRUE)
+  
+  
+  for (j in 1:length(data)) {
+    result <- fit(
+      data = data[[j]],
+      family = family[j], 
+      design = design,
+      rse_type = "classic",
+      offset = offset,
+      ss_correct = T
+    )
+    table <- summary_table(result)
+    file_name1 <- paste0(dataname, "_model", j, "_", result$family, ".rds")
+    file_name2 <- paste0(dataname, "_table", j, "_", result$family, ".pdf")
+    result$abbr <- paste(c("itm = immediate treatment model", 
+                               "etim = exposure-time indicator model", 
+                               "tehm = treatment-exposure heterogeneity model", 
+                               "ncsm = natural cubic spline model",
+                               "reest = random effect estimates", 
+                               "est = estimated average treatment effect",
+                               "se = standard error", 
+                               "rse = robust standard error", 
+                               "ci = confidence interval",
+                               "lte = long-term treatment effect"))
+    setwd(drive_folder)
+    saveRDS(result, file_name1)
+    html_file <- tempfile(fileext = ".html")
+    saveWidget(table, html_file)
+    webshot(html_file, file_name2)
+  }
+  
   
 }
+
