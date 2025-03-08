571310758c676d73d6c725208fe76bb713a1c98d
douyangyd <dyou6561@gmail.com>
2025-03-07 22:20:49 -0500

    Update to allow different offsets for different Poisson outcomes

--- Data_processing.R
+++ Data_processing.R
@@ -1,61 +1,66 @@
-# Data Processing
-process_data <- function(data) {
+# Identify different types of outcome variables
+outcome_columns <- grep("out", names(data), value = TRUE)
+off_columns <- grep("off", names(data), value = TRUE)
+ind_columns <- grep("id_individual", names(data), value = TRUE)
+out_poiss_vars <- grep("out_poiss", names(data), value = TRUE)  # Poisson outcomes
+out_other_vars <- setdiff(grep("out_", names(data), value = TRUE), out_poiss_vars)  # Other outcomes
 
-### Create a list to hold copies of the dataset
-dataset_copies <- list()
 
-### Loop through each outcome column to create a copy of the dataset
-### The outcome of this function is a list of datasets (length of the list equals to the number of columns)
-### Each dataset contains one outcome with all other variables that we need for model fitting
-### I choose this way to avoid the dynamic changes of outcome variable names when fit the model 
-for (outcome in outcome_columns) {
-  dataset_copy <- data
-  dataset_copy$outcome <- dataset_copy[[outcome]]
-  dataset_copy <- dataset_copy[, !names(dataset_copy) %in% outcome_columns]  # Drop original outcome columns
-  dataset_copies[[outcome]] <- dataset_copy
-}
-
-### Display the copies
-dataset_copies
-
-### Data formulation
-data.format <- function(dataset_copies){
-  data <- list()
-  for (i in 1:length(dataset_copies)){
-    data[[i]] <- dplyr::rename(dataset_copies[[i]], Outcome = outcome, Cluster = id_cluster, Treatment = trt, Period = time, Exposure = time_on_trt)
-    data[[i]][,c("Cluster", "Period")] <- lapply(data[[i]][,c("Cluster", "Period")], factor)
+process_data <- function(data){
+  # Create the list of datasets
+  dataset_list <- list()
+  
+  # Process Poisson outcome variables
+  for (outcome in out_poiss_vars) {
+    suffix <- sub("out_poiss_", "", outcome)
+    offset_var <- paste0("off_poiss_", suffix)
+    #dataset_list$outcome <- dataset_list[[outcome]]
+    
+    if (offset_var %in% names(data)) {
+      datacopy <- data
+      datacopy$outcome <- datacopy[[outcome]]
+      datacopy$offset <- datacopy[[offset_var]]
+      datacopy <- datacopy[, !names(data) %in% c(outcome_columns,off_columns)]
+      dataset_list[[outcome]] <- datacopy
+    } else {
+      datacopy <- data
+      datacopy$outcome <- datacopy[[outcome]]
+      datacopy <- datacopy[, !names(data) %in% c(outcome_columns,off_columns)]
+      dataset_list[[outcome]] <- datacopy
+    }
   }
+  
+  # Process other outcome variables (binary, continuous)
+  for (outcome in out_other_vars) {
+    datacopy <- data
+    datacopy$outcome <- datacopy[[outcome]]
+    datacopy <- datacopy[, !names(data) %in% c(outcome_columns,off_columns)]
+    dataset_list[[outcome]] <- datacopy
+  }
+  
+  # Print structure of the list
+  #str(dataset_list)
+  
+  data.format <- function(dataset_list){
+    data <- list()
+    for (i in 1:length(dataset_list)){
+      data[[i]] <- dplyr::rename(dataset_list[[i]], Outcome = outcome, Cluster = id_cluster, Treatment = trt, Period = time, Exposure = time_on_trt)
+      data[[i]][,c("Cluster", "Period")] <- lapply(data[[i]][,c("Cluster", "Period")], factor)
+    }
+    return(data)
+  }
+  
+  ## Return the list of the dataset
+  data <- data.format(dataset_list)
+  
   return(data)
 }
 
-## Return the list of the dataset
-data <- data.format(dataset_copies)
-
-return(data)
-
-}
-### Identify outcome columns (assuming outcome columns are named 'outcome1', 'outcome2', etc.)
-outcome_columns <- grep("out", names(data), value = TRUE)
-
-### number of binary outcome
-binary_columns <-grep("bin", names(data), value=TRUE)
-### number of continuous outcome
-gaussian_columns <-grep("con", names(data), value=TRUE)
-### number of count outcome
-poisson_columns <-grep("poiss", names(data), value=TRUE)
-
-
-### Identify offset columns (only for count outcomes, for other outcomes, this is always 0)
-offset_columns <- grep("off", names(data), value = TRUE)
-
-### Identify indiivudal ID columns  
-ind_columns <- grep("id_individual", names(data), value = TRUE)
 data <- process_data(data)
+
 
 family <- sapply(outcome_columns, function(x) {
   if (grepl("poiss", x)) return("poisson")
   if (grepl("bin", x)) return("binomial")
   if (grepl("con", x)) return("gaussian")
 })
-
-
