bd642c41c002a202d54b836e1839e0b73a6addc1
douyangyd <dyou6561@gmail.com>
2025-03-25 20:45:32 -0400

    adding rse to spline code

--- helpers.R
+++ helpers.R
@@ -181,34 +181,85 @@ get_ncscoef <- function(model, data, rse_type, ss_correct){
   model_se <- model$te_se
   
   # Still working on implementing the robust standard error
-  model_rse <- NA
-  rse.matrix <- NA
+  S <- max(data$exposure_time)
+  knots_exp <- seq(0, S, length.out=n_knots)
+  ns_basis <- splines::ns(
+    x = data$exposure_time,
+    knots = knots_exp[2:(n_knots-1)],
+    intercept = TRUE,
+    Boundary.knots = knots_exp[c(1,n_knots)]
+  )
   
+  # Specify the indices corresponding to the spline terms
+  indices <- grep("^b[0-9]+$", rownames(model_summary$coefficients))
+  index_max <- length(indices)
+  
+  # Extract coefficient estimates and covariance matrix corresponding to spline
+  # terms
+  coeffs_spl <-  model_summary$coefficients[,1][indices]
+  rse.matrix <- model_rse <- vcovCR.glmerMod(model$model, type = rse_type)
+  sigmarse.matrix <- model_rse[indices,indices]
+  sigmarse.matrix <- B %*% sigmarse.matrix %*% t(B)
+  
+  # Get number of unique (non-zero) exposure times
+  exp_timepoints <- unique(data$exposure_time[data$exposure_time != 0])
+  num_exp_timepoints <- length(exp_timepoints)
+  max_exp_timepoint <- max(exp_timepoints)
+  
+  # Transform the spline terms into effect curve estimates (+ covariance matrix)
+  B <- as.matrix(splines::ns(
+    x = c(1:S),
+    knots = knots_exp[2:(n_knots-1)],
+    intercept = TRUE,
+    Boundary.knots = knots_exp[c(1,n_knots)]
+  ))
+  
+  class(B) <- "matrix"
+  
+  rse.matrix <- model_rse <- vcovCR.glmerMod(model$model, type = rse_type)
+  sigmarse.matrix <- model_rse[indices,indices]
+  sigmarse.matrix <- B %*% sigmarse.matrix %*% t(B)
+  rse_ncs <- sqrt(diag(matrix(sigmarse.matrix, nrow = nrow(sigmarse.matrix))))
+  
+  
+  num_estimand_timepoints <- max(exp_timepoints)
+  M <- matrix(rep(1/num_exp_timepoints, num_exp_timepoints), nrow=1)
+  model_rse <- (sqrt(M %*% sigmarse.matrix %*% t(M)))[1,1]
+  
+
+  
+  #se_ncs <- sqrt(diag(matrix(cov_mtx, nrow = nrow(cov_mtx))))
+  lte_ind <- which.max(model$effect_curve$exp_time)
+  lte <- model$effect_curve$est[lte_ind]
+  lte_se <-  model$effect_curve$se[lte_ind]
+  lte_rse <- rse_ncs[lte_ind]
   ### obtain CI depending on SS correction
   if (ss_correct == T){
     dof <- model_summary$ngrps[["cluster_id"]] - 2
     modelci <- cal_confint_t(pe = model_est, df = dof, se = model_se)
     modelci_rse <- cal_confint_t(pe = model_est, df = dof, se = model_rse)
+    lteci <- cal_confint_t(pe = lte, df = dof, se = lte_se)
+    lteci_rse <- cal_confint_t(pe = lte, df = dof, se = lte_rse)
   } else {
     modelci <- model_est + c(-1.96,1.96) * model_se
     modelci_rse <- model_est + c(-1.96,1.96) * model_rse
+    lteci <- lte + c(-1.96,1.96) * lte_se
+    lteci_rse <- lte + c(-1.96,1.96) * lte_rse
   }
   
-  lte_ind <- which.max(model$effect_curve$exp_time)
   list <- list(
     model = model$model,
-    lte = "NA",
     reest = format_reest(model_reest),
     est = model_est,
     se = model_se,
     rse = model_rse,
     ci = modelci,
     rseci = modelci_rse,
-    lte = model$effect_curve$est[lte_ind],
-    lte_se = model$effect_curve$se[lte_ind],
-    lte_rse = "NA",
-    lteci = c(model$effect_curve$ci_lower[lte_ind],model$effect_curve$ci_upper[lte_ind]),
-    lterseci = "NA",
+    lte = lte,
+    lte_se = lte_se,
+    lte_rse = lte_rse,
+    lteci = lteci,
+    lterseci = lteci_rse,
     vcov_rv = rse.matrix,
     converged = performance::check_convergence(model$model)[1],
     lme4_converged = ifelse(model$model@optinfo$conv$opt==0, TRUE, FALSE),
