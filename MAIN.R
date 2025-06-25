d9f1c7c0f44b9a723cb6309b6e31a789477a5dfb
Derek Ouyang <douyang@cheos.ubc.ca>
2025-06-24 23:44:13 -0400

    update

--- MAIN.R
+++ MAIN.R
@@ -4,12 +4,13 @@ cfg <- list(
   quiet = T,
   pkgs = c("dplyr", "msm", "lmerTest", "splines", "performance", "stringr",
            "sandwich", "clubSandwich", "WoodburyMatrix", "steppedwedge",
-           "htmltools", "webshot", "DT", "here"),
-  path = "G:/Shared drives/Stepped Wedge Data Files/Trials/",
-  ind = c(5) # This corresponds to the datasets in dir(cfg$path)
+           "htmltools", "webshot", "DT", "here", "ggplot2"),
+  #path = "G:/Shared drives/Stepped Wedge Data Files/Trials/",
+  path = "~/Library/CloudStorage/GoogleDrive-derek6561@gmail.com/Shared drives/Stepped Wedge Data Files/Trials/",
+  ind = c(10) # This corresponds to the datasets in dir(cfg$path)
   # ind = c(1:74)
 )
-
+# c(9,15:17,21,26:28)
 # Load packages
 # To install `steppedwedge`, run the following:
 #     devtools::install_github(repo="Avi-Kenny/steppedwedge", dependencies=T)
@@ -22,12 +23,14 @@ for (pkg in c(cfg$pkgs)) {
 }
 
 # Source necessary files
-here::i_am("swcrtreanalysis/MAIN.R")
-setwd(paste0(here::here(), "/swcrtreanalysis"))
+#here::i_am("swcrtreanalysis/MAIN.R")
+#setwd(paste0(here::here(), "/swcrtreanalysis"))
+#setwd("G:/Shared drives/Stepped Wedge Data Files/Software/Data Analysis")
+setwd("~/Library/CloudStorage/GoogleDrive-derek6561@gmail.com/Shared drives/Stepped Wedge Data Files/Software/Data Analysis")
 files <- c("data_analysis.R", "helpers.R", "process results.R")
 for (file in files) { do.call("source", list(file)) }
-source("G:/Shared drives/Stepped Wedge Data Files/Software/Robust Variance/vcovCRglmerMod.R") # Temporary
-
+#source("G:/Shared drives/Stepped Wedge Data Files/Software/Robust Variance/vcovCRglmerMod.R") # Temporary
+source("~/Library/CloudStorage/GoogleDrive-derek6561@gmail.com/Shared drives/Stepped Wedge Data Files/Software/Robust Variance/vcovCRglmerMod.R")
 # Loop through datasets and run analysis
 for (i in cfg$ind) {
   
@@ -36,7 +39,7 @@ for (i in cfg$ind) {
   ))
   dataname <- print(dir(cfg$path)[i])
   head(data)
-  source("Data_processing.R")
+  source("~/Library/CloudStorage/GoogleDrive-derek6561@gmail.com/Shared drives/Stepped Wedge Data Files/software/Data Analysis/Data_processing.R")
   # source("~/OneDrive/SickKids/Method Project/TimedependentRVE/swcrtreanalysis2/data_processing_test.R") 
   # number of outcomes in this dataset
   length(outcome_columns)
@@ -60,12 +63,15 @@ for (i in cfg$ind) {
       family = family[j], 
       design = design,
       rse_type = "MD",
-      offset = !is.null(data[[j]]$offset),
+      offset = if(is.null(data[[j]]$offset)==T){NULL}else{data[[j]]$offset},
+      aggregate = F,
       ss_correct = T
     )
     table <- summary_table(result)
-    file_name1 <- paste0(dataname, "_model", j, "_", result$family, ".rds")
-    file_name2 <- paste0(dataname, "_table", j, "_", result$family, ".pdf")
+    plot <- plot_design(swdat)$design_plot
+    file_name1 <- paste0(dataname, "_model", "_", outcome_columns[j], ".rds")
+    file_name2 <- paste0(dataname, "_table", "_", outcome_columns[j], ".pdf")
+    file_name3 <- paste0(dataname, "_plot", "_", outcome_columns[j], ".pdf")
     result$abbr <- paste(c("itm = immediate treatment model", 
                                "etim = exposure-time indicator model", 
                                "tehm = treatment-exposure heterogeneity model", 
@@ -76,12 +82,14 @@ for (i in cfg$ind) {
                                "rse = robust standard error", 
                                "ci = confidence interval",
                                "lte = long-term treatment effect"))
+    #browser()
     result$design <- design
     setwd(drive_folder)
     saveRDS(result, file_name1)
     html_file <- tempfile(fileext = ".html")
     saveWidget(table, html_file)
     webshot(html_file, file_name2)
+    ggsave(file_name3, plot, width=8, height=11, units="in", dpi=400)
   }
 }
 
